4227628e05ea7238c83cc50455eae1a1810bdc73
David Dehne <dehne.david@gmail.com>
2025-01-22 21:26:14 -0500

    captcha cleanup

--- js/contact_me.js
+++ js/contact_me.js
@@ -9,10 +9,17 @@ $(function() {
             event.preventDefault(); // prevent default submit behaviour
 
             // Captcha validation
+            const captchaFailure = function() {
+                $('#success').html("<div class='alert alert-danger'>");
+                $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                    .append("</button>");
+                $('#success > .alert-danger').append("<strong>Please provide proof of humanity!</strong>");
+                $('#success > .alert-danger').append('</div>');
+            }
             const form = document.getElementById('contactForm');
             const hCaptcha = form.querySelector('textarea[name=h-captcha-response]').value;
             if (!hCaptcha) {
-                console.log('REJECTED')
+                captchaFailure()
                 return
             }
             // get values from FORM
@@ -37,6 +44,7 @@ $(function() {
                     .append('</div>');
         
                 //clear all fields
+                hcaptcha.reset("captcha-el")
                 $('#contactForm').trigger("reset");
             }
         
@@ -48,6 +56,7 @@ $(function() {
                 $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
                 $('#success > .alert-danger').append('</div>');
                 //clear all fields
+                hcaptcha.reset("captcha-el")
                 $('#contactForm').trigger("reset");
             }
             
@@ -75,42 +84,6 @@ $(function() {
 
     
             });
-
-            // $.ajax({
-            //     url: "https://api.web3forms.com/submit",
-            //     type: "POST",
-            //     data: {
-            //         name: name,
-            //         phone: phone,
-            //         email: email,
-            //         message: message,
-            //         access_key: 'd950d18e-3a7f-42e9-8dce-6575a502c997',
-            //     },
-            //     cache: false,
-            //     success: function() {
-            //         // Success message
-            //         $('#success').html("<div class='alert alert-success'>");
-            //         $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-            //             .append("</button>");
-            //         $('#success > .alert-success')
-            //             .append("<strong>Your message has been sent. </strong>");
-            //         $('#success > .alert-success')
-            //             .append('</div>');
-
-            //         //clear all fields
-            //         $('#contactForm').trigger("reset");
-            //     },
-            //     error: function() {
-            //         // Fail message
-            //         $('#success').html("<div class='alert alert-danger'>");
-            //         $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-            //             .append("</button>");
-            //         $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
-            //         $('#success > .alert-danger').append('</div>');
-            //         //clear all fields
-            //         $('#contactForm').trigger("reset");
-            //     },
-            // })
         },
         filter: function() {
             return $(this).is(":visible");
